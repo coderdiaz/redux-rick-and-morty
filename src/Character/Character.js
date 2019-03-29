234183026608fb117198f2e846ad768b1754c22f
Yonatan Rojas S <yonatan@bedu.org>
2019-03-28 21:50:44 -0600

    passed props as a Object

--- src/Character/Character.js
+++ src/Character/Character.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Character = (props) => {
+const Character = ({name, age, hobbie}) => {
   return (
     <>
-      <h1>Hola soy {props.name}, y tengo {props.age}</h1>
-      <h2>y mi hobbie es {props.hobbie}</h2>
+      <h1>Hola soy {name}, y tengo {age}</h1>
+      <h2>y mi hobbie es {hobbie}</h2>
     </>
   )
 }
