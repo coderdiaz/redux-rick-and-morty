ceba224356730ee1104a92ce59e38a14616ee966
Yonatan Rojas S <yonatan@bedu.org>
2019-03-28 22:57:03 -0600

    fetch single character

--- src/App.js
+++ src/App.js
@@ -3,15 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 import Character from './Character/Character';
 
+import axios from 'axios';
+
 class App extends Component {
+
+  state = {
+    characters: []
+  }
+
+  componentDidMount() {
+    this.getCharacter()
+    console.log('state from componentDidMount: ', this.state)
+  }
+
+  async getCharacter() {
+    let random = Math.floor(Math.random() * 100 + 1)
+    try {
+      const res = await axios.get(`https://rickandmortyapi.com/api/character/${random}/`)
+      console.log('characters: ', res.data)
+      this.setState({ characters: res.data})
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }
+
   render() {
+    console.log('state from render: ', this.state)
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hola, Vine a fotar
-          </p>
+          <img src={this.state.characters.image || logo} className="App-logo" alt="logo" />
+          <button onClick={() => this.getCharacter()}>Random character</button>
           <Character />
           <a
             className="App-link"
