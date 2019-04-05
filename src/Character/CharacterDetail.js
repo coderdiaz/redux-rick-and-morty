bce5d8eea1972a2cd17351c3d96c3e719bec629d
Yonatan Rojas S <yonatan@bedu.org>
2019-04-05 00:31:01 -0600

    display character id info

--- src/Character/CharacterDetail.js
+++ src/Character/CharacterDetail.js
@@ -3,11 +3,37 @@ import axios from 'axios'
 
 class CharacterDetail extends Component {
 
+  state = {
+    image: '',
+    name: '',
+    origin: ''
+  }
+
+  componentDidMount(){
+    this.getCharacter()
+  }
+
+  async getCharacter() {
+    try {
+      const res = await axios.get(`https://rickandmortyapi.com/api/character/${this.props.match.params.id}`)
+      // console.log('characters: ', res.data)
+      this.setState({
+        image: res.data.image,
+        name: res.data.name,
+        origin: res.data.origin.name
+      })
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
   render() {
     return (
       <>
-        <h1>name</h1>
-        <p>Hola Mucho gusto!!!!</p>
+        <img src={this.state.image} alt=""/>
+        <h1>{this.state.name}</h1>
+        <p>{this.state.origin}</p>
       </>
     )
   }
