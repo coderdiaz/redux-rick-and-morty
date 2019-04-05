511422250b85baa3ff56e8eaa1b49b4a37cda0a3
Yonatan Rojas S <yonatan@bedu.org>
2019-04-04 23:30:27 -0600

    passed logic from App to CharacterContainer

---
+++ src/Character/CharacterContainer.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import Character from './Character'
+
+class CharacterContainer extends Component {
+
+  state = {
+    characters: []
+  }
+
+  componentDidMount() {
+    this.getCharacter()
+    // console.log('state from componentDidMount: ', this.state)
+  }
+
+  async getCharacter() {
+    // let random = Math.floor(Math.random() * 100 + 1)
+    try {
+      // const res = await axios.get(`https://rickandmortyapi.com/api/character/${random}/`)
+      const res = await axios.get(`https://rickandmortyapi.com/api/character/`)
+      // console.log('characters: ', res.data.results)
+      this.setState({ characters: res.data.results })
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  render() {
+    let characters = this.state.characters.map(character => {
+      return <Character
+        key={character.id}
+        image={character.image}
+        name={character.name}
+        gender={character.gender}
+        specie={character.species}
+      />
+    })
+    // console.log('state from render: ', this.state)
+    return (
+      <div className="App">
+        <header className="App-header">
+          {characters}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default CharacterContainer
