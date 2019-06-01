362adfb8b20e3f6286730cb6805e673eb9b1cc0f
Yonatan Rojas S <yonatan@bedu.org>
2019-06-01 00:30:43 -0500

    created fetchCharactersAsync

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,4 +1,8 @@
 import { FETCH_CHARACTERS_SUCCESS, FETCH_CHARACTERS_ERROR } from './types'
+import axios from 'axios'
+
+
+const BASE_URL = 'https://rickandmortyapi.com/api/character/'
 
 // charactersReducer
 export const fetchCharactersSuccess = characters => ({
@@ -10,4 +14,17 @@ export const fetchCharactersError = err => ({
   type: FETCH_CHARACTERS_ERROR,
   payload: err,
   error: true
-})
+});
+
+export const fetchCharactersAsync = () => {
+  return async dispatch => {
+    try {
+      res = await axios.get(BASE_URL)
+      console.log('response: ', res.data)
+      // dispatch(fetchCharactersSuccess(res.data))
+    } catch(err) {
+      dispatch(fetchCharactersError(err))
+    }
+  }
+}
+
